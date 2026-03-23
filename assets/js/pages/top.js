33e1b74a473cfc6c573250693a43d24ac685d680
西田香子 <k-nishida@jutjoy.com>
2026-03-23 22:18:46 +0900

    css/js記載__20260323_nishida

--- assets/js/pages/top.js
+++ assets/js/pages/top.js
@@ -4,5 +4,13 @@ const swiper = new Swiper('.swiper', {
   loop: true,
   autoplay: {
     delay:3000
-  }
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation:{
+    nextEl:'.swiper-button-next',
+    prevEl:'.swiper-button-prev',
+  },
 });
+
